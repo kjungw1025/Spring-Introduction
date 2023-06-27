782f9764b7944b8ef00be6487bf24e9d052b3498
kjungw1025 <kjungw1025@naver.com>
2023-06-27 12:50:31 +0900

    feat: Update HelloController.java
    Add api method

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
@@ -18,5 +20,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name") String name, Model model) {
         model.addAttribute("name", name); //parameter로 넘어온 name을 넘겨줌
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // HTTP protocol의 body부분에 return 값을 직접 넣어주겠다는 의미 (ex. "hello spring")
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+    // 데이터를 그대로 내려줌
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
