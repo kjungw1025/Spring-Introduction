14a5b7ec116ac84017ceda4edfbb7236b8240aed
kjungw1025 <kjungw1025@naver.com>
2023-06-27 12:11:55 +0900

    feat: Update HelloController.java
    Create new controller hello-mvc

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
     public String hello (Model model) {
         model.addAttribute("data", "hello!!");  // attributeName: "data", attributeValue: "hello!!"
         return "hello"; // templates > hello.html를 찾아서 랜더링하라는 의미
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model) {
+        model.addAttribute("name", name); //parameter로 넘어온 name을 넘겨줌
+        return "hello-template";
     }
 }
