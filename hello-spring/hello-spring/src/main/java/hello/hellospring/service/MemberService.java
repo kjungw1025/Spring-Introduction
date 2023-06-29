7bc5ce9dbd505e5de66dbf4a7659bb9f41c01cc8
kjungw1025 <kjungw1025@naver.com>
2023-06-29 23:01:58 +0900

    feat: Update MemberService.java
    change code because of Dependency Injection (DI)

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,12 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    
+    // 외부에서 입력하도록
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     // 회원 가입
     public Long join(Member member) {
