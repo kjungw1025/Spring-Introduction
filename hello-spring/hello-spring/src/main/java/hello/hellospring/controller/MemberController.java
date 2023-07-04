de2409770e00b73711b4e0f7f74b02e52bbae7e1
kjungw1025 <kjungw1025@naver.com>
2023-07-04 23:21:41 +0900

    feat: Create Member management mvc

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -31,5 +37,27 @@ public class MemberController {
     @Autowired  // Autowired를 하면, 스프링 컨테이너에서 memberService를 가져다가 연결해줌
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String CreateForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create (MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName()); //form양식 안의 input의 "name"을 가져와서 setter 수행
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); // member의 list 자체들을 model에 다 담아줌
+        return "members/memberList";
     }
 }
